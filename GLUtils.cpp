f45b0a465ec51859ca8be665e67319fbe7cf078f
Palanath <pala@dusttoash.org>
2023-08-05 23:24:50 -0500

    Add GLUtils

---
+++ GLUtils.cpp
@@ -0,0 +1,10 @@
+/*
+ * GLUtils.cpp
+ *
+ *  Created on: Aug 5, 2023
+ *      Author: Palanath
+ */
+
+#include "GLUtils.hpp"
+
+
---
+++ GLUtils.hpp
@@ -0,0 +1,18 @@
+/*
+ * GLUtils.hpp
+ *
+ *  Created on: Aug 5, 2023
+ *      Author: Palanath
+ */
+
+#ifndef GLUTILS_HPP_
+#define GLUTILS_HPP_
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+auto createShader(GLenum shaderType, const char* source);
+
+
+#endif /* GLUTILS_HPP_ */
