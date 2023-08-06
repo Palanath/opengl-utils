6932f232a2dc9cec08a0108289a815d2bc8dde0e
Palanath <pala@dusttoash.org>
2023-08-05 23:58:39 -0500

    Update GLUtils

--- GLUtils.cpp
+++ GLUtils.cpp
@@ -7,4 +7,15 @@
 
 #include "GLUtils.hpp"
 
+GLuint createShader(GLenum shaderType, const char *source) {
+	GLuint res = glCreateShader(shaderType);
+	glShaderSource(res, 1, &source, nullptr);
+	glCompileShader(shaderType);
+	return res;
+}
 
+bool checkShaderCompilationError(GLuint shader) {
+	int success;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	return success;
+}
--- GLUtils.hpp
+++ GLUtils.hpp
@@ -11,8 +11,15 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-
-auto createShader(GLenum shaderType, const char* source);
-
+/*
+ * Creates a shader with the specified type and source code, and compiles it.
+ * Errors can be checked for with checkShadercompilationError(GLuint).
+ */
+GLuint createShader(GLenum shaderType, const char *source);
+/*
+ * Checks if there are shader errors.
+ * Returns true if there are and false otherwise.
+ */
+bool checkShaderCompilationError(GLuint shader);
 
 #endif /* GLUTILS_HPP_ */
